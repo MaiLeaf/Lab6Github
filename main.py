39680b3ec75f374549e667731f2cab9e6c618d2f
jillhwg <133899545+jillhwg@users.noreply.github.com>
2023-07-18 14:43:56 -0400

    added comment

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
 '''
 jill's comment
 '''
 print("testing testing")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
